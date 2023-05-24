1c9506259c0331399aa2911701c7135cf4fe5c95
Brian-M-J <120668309+Brian-M-J@users.noreply.github.com>
2023-05-24 20:23:37 +0530

    Add files via upload

---
+++ findat/main.py
@@ -0,0 +1,73 @@
+import os
+import fastcoref
+import spacy
+from fastcoref import spacy_component
+
+
+def coref_resolution(file_path: str) -> str:
+    '''
+        This function accepts a file path as input, opens the file, and
+        performs coreference resolution on it using the fastcoref module.
+    '''
+    nlp: spacy.lang.en.English = spacy.load("en_core_web_sm")
+    nlp.add_pipe("fastcoref")
+    text: str
+    with open(file_path) as f:
+        text = f.read()
+    doc: spacy.tokens.doc.Doc = nlp(
+       text, 
+       component_cfg={"fastcoref": {'resolve_text': True}}
+    )
+    return doc._.resolved_text
+
+
+def process_directory(directory_path: str, key_string: str) -> dict[str, int]:
+    '''
+        This function processes all the text files in a given directory.
+    '''
+    file_occurrences: dict[str, int] = {}
+    file_name: str
+    for file_name in os.listdir(directory_path):
+        if file_name.endswith(".txt"):
+            file_path: str = os.path.join(directory_path, file_name)
+            resolved_text: str = coref_resolution(file_path)
+            occurrences: int = resolved_text.lower().count(key_string.lower())
+            file_occurrences[file_name] = occurrences
+    return file_occurrences
+
+
+def sort_files_by_occurrences(directory_path: str, key_string: str) -> tuple[str, int]:
+    '''
+        Function to generate a list of file names in descending order
+        based on the number of occurrences of the key string.
+    '''
+    file_occurrences: dict[str, int] = process_directory(directory_path, key_string)
+    return sorted(
+        file_occurrences.items(), key=lambda x: x[1], reverse=True
+    )
+
+
+# Example usage
+def main() -> None:
+    '''
+        Process all text files in a directory specified by the user.
+        Output a list of file names in descending order of the number of times
+        they occur in the files.
+    '''
+    directory_path: str = input(
+        "Please enter the directory where the text files to be scanned are kept: "
+    )
+    key_string: str = input("\nPlease enter the key string to search in the files: ")
+    sorted_files: tuple[str, int] = sort_files_by_occurrences(directory_path, key_string)
+    print(
+        "\nHere are the files with the key string contained. Their names are given in descending order of the number of times the key string occurred in the files:\n"
+    )
+    file: str
+    occurrences: int
+    for file, occurrences in sorted_files:
+        if occurrences > 0:
+            print(f'{file}: {occurrences} occurrences')
+
+
+if __name__ == "__main__":
+    main()
